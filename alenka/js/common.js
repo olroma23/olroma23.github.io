1961323c3693aabe99fae69f11d2742d50a5ca57
Roman Oliinyk <roliynyk68@gmail.com>
2018-05-28 00:10:50 +0300

    alenka final1

--- alenka/js/common.js
+++ alenka/js/common.js
@@ -45,6 +45,25 @@ $(function() {
 	$(".scroll").mPageScroll2id({
 		offset: 175
 	});
+
+
+
+
+$("form.callback").submit(function() { //Change
+	var th = $(this);
+	$.ajax({
+		type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+			setTimeout(function() {
+				$(th).find('.success').removeClass('active').fadeOut();
+				th.trigger("reset");
+			}, 3000);
+		});
+		return false;
+	});
 	
 });
 
