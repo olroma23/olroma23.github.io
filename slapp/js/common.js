3a474e31e11cff7b05ea325e567c8482807a5d6f
Roman Oliinyk <roliynyk68@gmail.com>
2018-06-21 01:26:34 +0300

    slapp header

---
+++ slapp/js/common.js
@@ -0,0 +1,48 @@
+$(function() {
+
+
+	$(".first-element").click(function(){
+		$(".dropdown-toggle").toggleClass("rotate-first-class");
+	});
+
+
+	$(".second-element").click(function(){
+		$(".dropdown-toggle").toggleClass("rotate-second-class");
+	});
+
+
+	$(".third-element").click(function(){
+		$(".dropdown-toggle").toggleClass("rotate-third-class");
+	});
+
+
+
+
+	$('.menu-btn').on('click', function(e) {
+		e.preventDefault;
+		$(this).toggleClass('menu-btn_active');
+	});
+
+
+	var vids = $("video"); 
+	$.each(vids, function(){
+		this.controls = false; 
+	}); 
+
+
+	$(window).scroll(function(){
+		$('.navbar-dark').toggleClass('navbar-active', $(this).scrollTop() > 250);
+	});
+
+
+	$(".menu-btn").click(function(){
+		$(".sidenav").toggleClass("sidenav-close")
+	});
+
+
+
+
+	// Custom JS
+
+});
+
