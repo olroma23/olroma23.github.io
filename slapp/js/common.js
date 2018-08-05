d9bdc93bd302df59fdf78110ed21f63916658106
Roman Oliinyk <roliynyk68@gmail.com>
2018-08-05 22:13:23 +0300

    final14

--- slapp/js/common.js
+++ slapp/js/common.js
@@ -318,8 +318,8 @@ $(function() {
 				var max, min;
 				var pos = $(this).scrollTop();
 				selectors.item.each(function(i) {
-					min = $(this).offset().top;
-					max = $(this).height() + $(this).offset().top;
+					min = $(this).offset().top - 150;
+					max = $(this).height() + $(this).offset().top - 150;
 					var that = $(this);
 					if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
 						selectors.item.removeClass(selectors.activeClass);
