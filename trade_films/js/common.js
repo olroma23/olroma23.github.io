244744873dd4f654b0b3b68079561834594e5b70
Roman Oliinyk <roliynyk68@gmail.com>
2018-10-10 00:56:43 +0300

    contact

--- trade_films/js/common.js
+++ trade_films/js/common.js
@@ -1,4 +1,40 @@
+
+
+
+
 $(function() {
+
+	function isVisible(elem) {
+
+		var coords = elem.getBoundingClientRect();
+
+		var windowHeight = document.documentElement.clientHeight;
+
+		var topVisible = coords.top > 0 && coords.top < windowHeight;
+		var bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+
+		return topVisible || bottomVisible;
+	}
+
+	function showVisible() {
+		var imgs = document.getElementsByTagName('img');
+		for (var i = 0; i < imgs.length; i++) {
+
+			var img = imgs[i];
+
+			var realsrc = img.getAttribute('realsrc');
+			if (!realsrc) continue;
+
+			if (isVisible(img)) {
+				img.src = realsrc;
+				img.setAttribute('realsrc', '');
+			}
+		}
+	}
+	window.onscroll = showVisible;
+	showVisible();
+
+
 
 
 
@@ -32,7 +68,31 @@ $(function() {
 		}
 	});
 
-
+	$('.certificates').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		nav: true,
+		autoplay: true,
+		autoplayTimeout: 12000,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 1
+			},
+			768:{
+				items: 1
+			},
+			992: {
+				items: 1
+			}, 
+			1200: {
+				items: 1
+			}
+		}
+	});
 
 	// Custom JS
 
@@ -40,8 +100,14 @@ $(function() {
 
 
 
-
 });
+
+
+
+
+
+
+
 
 $(document).mouseup(function (e) {
 	var container = $(".sidenav");
